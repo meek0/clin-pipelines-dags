dc0ab652da6203a9038245d3eb37af9fa5212d4f
Patrice Laplante <plaplante@ferlab.bio>
2020-10-22 19:18:36 -0400

    Automation of creation of connection to S3

--- docker/scripts/entrypoint.sh
+++ docker/scripts/entrypoint.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 airflow initdb
 airflow webserver
+airflow connections --add --conn_id 'clin_object_store' --conn_login 'minio' --conn_password 'minio123' --conn_type 's3' --conn_extra '{"host":"http://192.168.0.16:9000"}'
