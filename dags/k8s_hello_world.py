c09ada984602deea4234830531d69b0751434a23
jecos <jcostanza@ferlab.bio>
2022-08-16 14:09:18 -0400

    test: add a conn_id

--- dags/k8s_hello_world.py
+++ dags/k8s_hello_world.py
@@ -13,6 +13,7 @@ default_args = {
 with DAG("k8s_hello_world", start_date=days_ago(2),
     schedule_interval=None, catchup=False) as dag:
         task_hello_world = KubernetesPodOperator(
+            conn_id='kubernetes',
             namespace='cqgc-etl',
             image='alpine',
             cmds=["sh", "-c", "echo 'Hello world!' "],
