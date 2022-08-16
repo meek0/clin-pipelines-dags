7c166da12a2d030d771a6fae3e486405867fc0d9
jecos <jcostanza@ferlab.bio>
2022-08-16 16:36:53 -0400

    test: hello world

--- dags/hello_world.py
+++ dags/hello_world.py
@@ -14,5 +14,5 @@ with DAG("hello_world", start_date=days_ago(2),
     schedule_interval=None, catchup=False) as dag:
         task_hello_world = BashOperator(
             task_id="hello_world",
-            bash_command="echo 'Hello World'",
+            bash_command="sleep 60 && echo 'Hello World'",
             dag=dag)
