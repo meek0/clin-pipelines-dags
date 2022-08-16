37d310d145661f85da7e693a9fd7c162448bc15c
jecos <jcostanza@ferlab.bio>
2022-08-16 13:53:34 -0400

    test: add context

--- dags/k8s_hello_world.py
+++ dags/k8s_hello_world.py
@@ -19,6 +19,7 @@ with DAG("k8s_hello_world", start_date=days_ago(2),
             name="say-hello",
             is_delete_operator_pod=False,
             in_cluster=True,
+            cluster_context= 'kubernetes-admin-cluster.etl.cqgc@cluster.etl.cqgc',
             task_id="say-hello",
             get_logs=True,
         )
