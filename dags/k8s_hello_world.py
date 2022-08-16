1f6d4a378be2937146c06b027c961f7f758d8104
jecos <jcostanza@ferlab.bio>
2022-08-16 14:31:18 -0400

    test: try to fix k8s_hello_world

--- dags/k8s_hello_world.py
+++ dags/k8s_hello_world.py
@@ -14,13 +14,9 @@ with DAG("k8s_hello_world", start_date=days_ago(2),
     schedule_interval=None, catchup=False) as dag:
         task_hello_world = KubernetesPodOperator(
             kubernetes_conn_id='kubernetes',
-            # namespace='cqgc-etl',
             image='alpine',
             cmds=["sh", "-c", "echo 'Hello world!' "],
             name="say-hello",
             is_delete_operator_pod=False,
-            # in_cluster=True,
-            # cluster_context= 'kubernetes-admin-cluster.etl.cqgc@cluster.etl.cqgc',
             task_id="say-hello",
-            # get_logs=True,
         )
