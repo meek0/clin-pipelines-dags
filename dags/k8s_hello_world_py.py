e3281827c86b7dcfd1109e73a9c3644b92f9c115
Issam <ihannache@ferlab.bio>
2022-08-16 18:25:46 -0400

    feat: INRA-80 update new dag name

--- dags/k8s_hello_world_py.py
+++ dags/k8s_hello_world_py.py
@@ -10,13 +10,13 @@ default_args = {
     "email": "jcostanza@ferlab.bio"
 }
 
-with DAG("k8s_hello_world", start_date=days_ago(2),
+with DAG("k8s_hello_world_Py", start_date=days_ago(2),
     schedule_interval=None, catchup=False) as dag:
         task_hello_world = KubernetesPodOperator(
             namespace='cqgc-etl',
             image="Python:3.6",
             cmds=["Python","-c"],
-            arguments=["print('hello world')"]
+            arguments=["print('hello world')"],
             name="test-say-hello",
             in_cluster=True,
             cluster_context= 'kubernetes-admin-cluster.etl.cqgc@cluster.etl.cqgc',
