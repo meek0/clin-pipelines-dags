e98578b3fed8e5d4765a2c40150c77a991ff0917
jecos <jcostanza@ferlab.bio>
2022-08-17 15:49:47 -0400

    test: list pod

--- dags/k8s_hello_world.py
+++ dags/k8s_hello_world.py
@@ -1,7 +1,9 @@
 from airflow import DAG
 from airflow.utils.dates import days_ago
 from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
-
+from airflow.operators.python import PythonOperator
+from kubernetes import client, config
+import logging
 default_args = {
     "owner": "jcostanza",
     "depends_on_past": False,
@@ -9,6 +11,14 @@ default_args = {
     "email_on_retry": False,
     "email": "jcostanza@ferlab.bio"
 }
+
+def _spark_task_check(ti):
+    v1 = client.CoreV1Api()
+    pod = v1.list_namespaced_pod(
+        namespace='cqgc-qa',
+        limit=3,
+    )
+    logging.info(pod)
 
 with DAG("k8s_hello_world", start_date=days_ago(2),
     schedule_interval=None, catchup=False) as dag:
@@ -21,3 +31,11 @@ with DAG("k8s_hello_world", start_date=days_ago(2),
             task_id="say-hello",
             get_logs=True,
         )
+
+        spark_task_check = PythonOperator(
+            task_id="spark_task_check",
+            python_callable=_spark_task_check,
+        )
+
+
+        task_hello_world >> spark_task_check
