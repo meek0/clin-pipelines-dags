ffc43a8c89be42e6ff0869c4bb439e1226ddbd06
jecos <jcostanza@ferlab.bio>
2022-08-17 15:21:24 -0400

    test: use conn id

--- dags/k8s_hello_world.py
+++ dags/k8s_hello_world.py
@@ -13,13 +13,11 @@ default_args = {
 with DAG("k8s_hello_world", start_date=days_ago(2),
     schedule_interval=None, catchup=False) as dag:
         task_hello_world = KubernetesPodOperator(
-            namespace='cqgc-qa',
+            kubernetes_conn_id='kubernetes',
             image='alpine',
             cmds=["sh", "-c", "echo 'Hello WOrld!'"],
             name="say-hello",
             is_delete_operator_pod=False,
-            in_cluster=True,
-            cluster_context= 'kubernetes-admin-cluster.etl.cqgc@cluster.etl.cqgc',
             task_id="say-hello",
             get_logs=True,
         )
