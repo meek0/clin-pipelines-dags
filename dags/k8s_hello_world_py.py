8f8298822a3f903f21a883e8f81d7e713d9905ba
Issam <ihannache@ferlab.bio>
2022-08-17 09:37:32 -0400

    feat: INRA-80 test another dag

--- dags/k8s_hello_world_py.py
+++ dags/k8s_hello_world_py.py
@@ -1,26 +1,36 @@
+from datetime import datetime, timedelta
+
 from airflow import DAG
-from airflow.utils.dates import days_ago
-from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
+from airflow.contrib.operators.kubernetes_pod_operator import \
+    KubernetesPodOperator
+from airflow.operators.dummy_operator import DummyOperator
 
 default_args = {
-    "owner": "jcostanza",
-    "depends_on_past": False,
-    "email_on_failure": False,
-    "email_on_retry": False,
-    "email": "jcostanza@ferlab.bio"
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime.utcnow(),
+    'email': ['airflow@example.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5)
 }
 
-with DAG("k8s_hello_world_Py", start_date=days_ago(2),
-    schedule_interval=None, catchup=False) as dag:
-        task_hello_world = KubernetesPodOperator(
-            namespace='cqgc-etl',
-            image="Python:3.6",
-            cmds=["Python","-c"],
-            arguments=["print('hello world')"],
-            name="test-say-hello",
-            in_cluster=True,
-            cluster_context= 'kubernetes-admin-cluster.etl.cqgc@cluster.etl.cqgc',
-            task_id="test-say-hello-world",
-            get_logs=True,
-            dag=dag
-        )
+dag = DAG(
+    'kubernetes_sample_1', default_args=default_args)
+
+
+start = DummyOperator(task_id='run_this_first', dag=dag)
+
+passing = KubernetesPodOperator(namespace='cqgc-etl',
+                                image="Python:3.6",
+                                cmds=["Python", "-c"],
+                                arguments=["print('hello world')"],
+                                labels={"foo": "bar"},
+                                name="passing-test",
+                                task_id="passing-task",
+                                get_logs=True,
+                                dag=dag
+                                )
+
+passing.set_upstream(start)
