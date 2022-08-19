3583594aee9352c229ef23dbc955a910de0ebf2c
Matt Syms <matthieu.symoens@gmail.com>
2022-08-19 07:41:57 -0400

    Updated hello world dag

--- dags/k8s_hello_world.py
+++ dags/k8s_hello_world.py
@@ -1,10 +1,13 @@
-from airflow import DAG
-from airflow.utils.dates import days_ago
-from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
-from airflow.operators.python import PythonOperator
-from kubernetes import client, config
-from airflow.models import Variable
+import kubernetes
 import logging
+from airflow import DAG
+from airflow.models import Variable
+from airflow.operators.python import PythonOperator
+from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
+from airflow.utils.dates import days_ago
+from lib.etl import config
+from lib.k8s import load_config as k8s_load_config
+
 default_args = {
     "owner": "jcostanza",
     "depends_on_past": False,
@@ -13,33 +16,39 @@ default_args = {
     "email": "jcostanza@ferlab.bio"
 }
 
-namespace=Variable.get("kubernetes_namespace")
+namespace = config.k8s_namespace
+
 
 def _spark_task_check(ti):
-    config.load_incluster_config()
-    v1 = client.CoreV1Api()
-    pod = v1.list_namespaced_pod(
+    k8s_load_config()
+    k8s_client = kubernetes.client.CoreV1Api()
+    pod = k8s_client.list_namespaced_pod(
         namespace=namespace,
         limit=3,
     )
     logging.info(pod)
 
-with DAG("k8s_hello_world", start_date=days_ago(2),
-    schedule_interval=None, catchup=False) as dag:
-        task_hello_world = KubernetesPodOperator(
-            namespace=namespace,
-            image='alpine',
-            cmds=["sh", "-c", "echo 'Hello WOrld!'"],
-            name="say-hello",
-            is_delete_operator_pod=False,
-            task_id="say-hello",
-            get_logs=True,
-        )
 
-        spark_task_check = PythonOperator(
-            task_id="spark_task_check",
-            python_callable=_spark_task_check,
-        )
+with DAG(
+    dag_id='k8s_hello_world',
+    start_date=days_ago(2),
+    schedule_interval=None,
+    catchup=False
+) as dag:
 
+    task_hello_world = KubernetesPodOperator(
+        namespace=namespace,
+        image='alpine',
+        cmds=["sh", "-c", "echo 'Hello WOrld!'"],
+        name="say-hello",
+        is_delete_operator_pod=False,
+        task_id="say-hello",
+        get_logs=True,
+    )
 
-        task_hello_world >> spark_task_check
+    spark_task_check = PythonOperator(
+        task_id="spark_task_check",
+        python_callable=_spark_task_check,
+    )
+
+    task_hello_world >> spark_task_check
