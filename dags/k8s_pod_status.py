68f34ad3b0d1f43b8ba76adbb0d1ee980a64aefd
Matt Syms <matthieu.symoens@gmail.com>
2022-08-22 19:13:23 -0400

    test: updated k8s_pod_status dag

--- dags/k8s_pod_status.py
+++ dags/k8s_pod_status.py
@@ -7,24 +7,22 @@ from lib.etl import config
 from lib.k8s import k8s_load_config
 
 
-def _pod_status():
-
-    k8s_load_config()
-    k8s_client = kubernetes.client.CoreV1Api()
-    list = k8s_client.list_namespaced_pod(
-        namespace=config.k8s_namespace,
-        limit=10,
-    )
-    logging.info('pods:')
-    for item in list.items:
-        logging.info(f'{item.metadata.name} ({item.status.phase})')
-
-
 with DAG(
     dag_id='k8s_pod_status',
     start_date=datetime(2022, 1, 1),
     schedule_interval=None,
 ) as dag:
+
+    def _pod_status():
+        k8s_load_config()
+        k8s_client = kubernetes.client.CoreV1Api()
+        list = k8s_client.list_namespaced_pod(
+            namespace=config.k8s_namespace,
+            limit=10,
+        )
+        logging.info('pods:')
+        for item in list.items:
+            logging.info(f'{item.metadata.name} ({item.status.phase})')
 
     k8s_pod_status = PythonOperator(
         task_id='pod_status',
