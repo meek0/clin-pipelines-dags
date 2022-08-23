2db2c5385c95ff9c5ff3fe538a875164f83b2116
Matt Syms <matthieu.symoens@gmail.com>
2022-08-23 08:59:29 -0400

    test: add k8s deployment pause, resume, restart

--- dags/lib/k8s.py
+++ dags/lib/k8s.py
@@ -1,5 +1,11 @@
 import kubernetes
 import os
+from airflow.models import Variable
+from airflow.operators.python import PythonOperator
+from datetime import datetime
+
+
+k8s_namespace = Variable.get('kubernetes_namespace')
 
 
 def k8s_load_config():
@@ -7,3 +13,68 @@ def k8s_load_config():
         kubernetes.config.load_kube_config()
     else:
         kubernetes.config.load_incluster_config()
+
+
+def k8s_deployment_pause(
+    task_id: str,
+    deployment: str,
+):
+    def _deployment_pause():
+        k8s_client = kubernetes.client.AppsV1Api()
+        k8s_client.patch_namespaced_deployment(
+            name=deployment,
+            namespace=k8s_namespace,
+            body={'spec': {'paused': True}},
+        )
+
+    return PythonOperator(
+        task_id=task_id,
+        python_callable=_deployment_pause,
+    )
+
+
+def k8s_deployment_resume(
+    task_id: str,
+    deployment: str,
+):
+    def _deployment_resume():
+        k8s_client = kubernetes.client.AppsV1Api()
+        k8s_client.patch_namespaced_deployment(
+            name=deployment,
+            namespace=k8s_namespace,
+            body={'spec': {'paused': False}},
+        )
+
+    return PythonOperator(
+        task_id=task_id,
+        python_callable=_deployment_resume,
+    )
+
+
+def k8s_deployment_restart(
+    task_id: str,
+    deployment: str,
+):
+    def _deployment_restart():
+        now = str(datetime.utcnow().isoformat('T') + 'Z')
+        k8s_client = kubernetes.client.AppsV1Api()
+        k8s_client.patch_namespaced_deployment(
+            name=deployment,
+            namespace=k8s_namespace,
+            body={
+                'spec': {
+                    'template': {
+                        'metadata': {
+                            'annotations': {
+                                'kubectl.kubernetes.io/restartedAt': now
+                            }
+                        }
+                    }
+                }
+            },
+        )
+
+    return PythonOperator(
+        task_id=task_id,
+        python_callable=_deployment_restart,
+    )
