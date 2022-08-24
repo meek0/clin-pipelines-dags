4b026ce946bc8fe28087f8415eeb0a341339fa94
Matt Syms <matthieu.symoens@gmail.com>
2022-08-24 08:32:53 -0400

    test: cleanup object property access

--- dags/k8s_pod_operator.py
+++ dags/k8s_pod_operator.py
@@ -14,7 +14,7 @@ with DAG(
         task_id='k8s_pod_operator',
         is_delete_operator_pod=True,
         namespace=config.k8s_namespace,
-        cluster_context=config.k8s_context['default'],
+        cluster_context=config.k8s_context.get('default'),
         name='k8s-pod-operator',
         image='alpine',
         cmds=['echo', 'hello'],
