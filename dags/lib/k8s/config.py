b6af5c8749536626cd34e1bf9c70aa094130a6a6
Matt Syms <matthieu.symoens@gmail.com>
2022-08-29 14:55:56 -0400

    test: load kube config

--- dags/lib/k8s/config.py
+++ dags/lib/k8s/config.py
@@ -7,7 +7,8 @@ k8s_namespace = Variable.get('kubernetes_namespace')
 
 
 def k8s_load_config():
-    if os.getenv('K8S_LOCAL_CONFIG', 'false') == 'true':
-        kubernetes.config.load_kube_config()
-    else:
-        kubernetes.config.load_incluster_config()
+    kubernetes.config.load_kube_config()
+    # if os.getenv('K8S_LOCAL_CONFIG', 'false') == 'true':
+        
+    # else:
+    #     kubernetes.config.load_incluster_config()
