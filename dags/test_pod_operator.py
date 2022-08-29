d5bd620befccbd81645d9178401adfd85f714793
Matt Syms <matthieu.symoens@gmail.com>
2022-08-29 16:21:35 -0400

    test: test pod operator

--- dags/test_pod_operator.py
+++ dags/test_pod_operator.py
@@ -13,9 +13,10 @@ with DAG(
     test_pod_operator = KubernetesPodOperator(
         task_id='test_pod_operator',
         is_delete_operator_pod=False,
-        namespace=config.k8s_namespace,
         in_cluster=False,
+        config_file='/home/airflow/.kube/config',
         cluster_context='airflow-cluster.qa.cqgc@cluster.qa.cqgc',
+        namespace=config.k8s_namespace,
         name='test-pod-operator',
         image='alpine',
         cmds=['echo', 'hello'],
