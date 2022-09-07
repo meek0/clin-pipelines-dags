dee45f4b746aec1bb0fd13116af2e8a56482e072
Matt Syms <matthieu.symoens@gmail.com>
2022-09-07 14:27:50 -0400

    feat: CLIN-1361 added reset enrich dags

---
+++ dags/etl_reset_enrich_variants.py
@@ -0,0 +1,25 @@
+from airflow import DAG
+from datetime import datetime
+from lib import config
+from lib.config import K8sContext
+from lib.operators.spark import SparkOperator
+
+
+with DAG(
+    dag_id='etl_reset_enrich_variants',
+    start_date=datetime(2022, 1, 1),
+    schedule_interval=None,
+) as dag:
+
+    env = config.environment
+
+    etl_reset_enrich_variants = SparkOperator(
+        task_id='etl_reset_enrich_variants',
+        name='etl-reset-enrich-variants',
+        k8s_context=K8sContext.ETL,
+        spark_class='bio.ferlab.clin.etl.enriched.RunEnriched',
+        spark_config='enriched-etl',
+        arguments=[
+            f'config/{env}.conf', 'initial', 'variants',
+        ],
+    )
