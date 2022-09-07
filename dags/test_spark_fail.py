52d34603968c757ba4e5e41930cdddfcfbe5560e
Matt Syms <matthieu.symoens@gmail.com>
2022-09-07 14:42:05 -0400

    feat: CLIN-1361 cleaned up test dags

--- dags/test_spark_fail.py
+++ dags/test_spark_fail.py
@@ -6,18 +6,18 @@ from lib.operators.spark import SparkOperator
 
 
 with DAG(
-    dag_id='test_spark_log',
+    dag_id='test_spark_fail',
     start_date=datetime(2022, 1, 1),
     schedule_interval=None,
 ) as dag:
 
-    test_spark_log = SparkOperator(
-        task_id='test_spark_log',
-        name='test-spark-log',
+    test_spark_fail = SparkOperator(
+        task_id='test_spark_fail',
+        name='test-spark-fail',
         k8s_context=K8sContext.ETL,
         spark_class='bio.ferlab.clin.etl.fail.Fail',
         spark_config='enriched-etl',
         arguments=[f'config/{config.environment}.conf', 'default'],
     )
 
-    test_spark_log
+    test_spark_fail
