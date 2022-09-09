3ed17f92bf3c71a082d0ae8315321a2bb89746a8
Matt Syms <matthieu.symoens@gmail.com>
2022-09-09 16:02:35 -0400

    feat: CLIN-1361 removed docker dir

--- docker/scripts/init.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-airflow initdb
-airflow connections --add --conn_id 'clin_object_store' --conn_login 'minio' --conn_password 'minio123' --conn_type 's3' --conn_extra '{"host":"http://minio:9000"}'
