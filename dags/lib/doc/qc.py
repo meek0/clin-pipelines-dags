9a21866a6cf20f0e89f396f4741ce3d58e17e566
Matt Syms <matthieu.symoens@gmail.com>
2022-09-19 13:51:34 -0400

    feat: added doc

---
+++ dags/lib/doc/qc.py
@@ -0,0 +1,33 @@
+etl_qc = '''
+# This is a title
+
+Lorem ipsum...
+
+## This is a sub-title
+
+- Hello
+- World
+
+### This is a sub-sub-title
+
+```
+this is a code block
+```
+'''
+
+no_dup_snv = '''
+# This is a title
+
+Lorem ipsum...
+
+## This is a sub-title
+
+- Hello
+- World
+
+### This is a sub-sub-title
+
+```
+this is a code block
+```
+'''
