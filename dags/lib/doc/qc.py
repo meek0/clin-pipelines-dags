397b3b2f21b6100a6a2f15aaba8993fd13d5e22d
Karine St-Onge <karinestonge@CRHSJ106508.local>
2022-09-20 06:40:18 -0400

    feat: CLIN-1379 Add doc to etl_qc dag and tasks

--- dags/lib/doc/qc.py
+++ dags/lib/doc/qc.py
@@ -1,33 +1,73 @@
 etl_qc = '''
-# This is a title
+# Tests critiques
 
-Lorem ipsum...
+L'échec d'un de ces tests bloque l'exécution du DAG.
 
-## This is a sub-title
+## Série de tests sur la non duplication des variants dans les tables
 
-- Hello
-- World
+### Fonctionnement des tests
+- Regrouper les variants selon la clé (chromosome, start, reference, alternate) et un critère supplémentaire selon le cas
+- Afficher les variants dupliqués
 
-### This is a sub-sub-title
+### Différents tests
+- Table normalized_snv
+- Table normalized_variants
+- Table variants
+- Table variant_centric
 
-```
-this is a code block
-```
+## Série de tests comparant la liste des variants entre les tables
+
+### Fonctionnement des tests
+- Lister les variants distincts selon la clé (chromosome, start, reference, alternate)
+- Comparer la liste de deux tables
+
+### Différents tests
+- Entre les tables normalized_snv et normalized_variants
+- Entre les tables normalized_snv et variants
+- Entre les tables variants et variant_centric
+
+---
+Pour plus de détails sur chaque test, voir "Task Instance Details".
 '''
 
 no_dup_snv = '''
-# This is a title
+### Documentation
+- Test : Non duplication - Table normalized_snv
+- Objectif : Les variants doivent être unique par patient_id dans la table normalized_snv
+'''
 
-Lorem ipsum...
+no_dup_nor_variants = '''
+### Documentation
+- Test : Non duplication - Table normalized_variants
+- Objectif : Les variants doivent être unique par batch_id dans la table normalized_variants
+'''
 
-## This is a sub-title
+no_dup_variants = '''
+### Documentation
+- Test : Non duplication - Table variants
+- Objectif : Les variants doivent être unique dans la table variants
+'''
 
-- Hello
-- World
+no_dup_variant_centric = '''
+### Documentation
+- Test : Non duplication - Table variant_centric
+- Objectif : Les variants doivent être unique dans la table variant_centric
+'''
 
-### This is a sub-sub-title
+same_list_snv_nor_variants = '''
+### Documentation
+- Test : Liste des variants - Entre les tables normalized_snv et normalized_variants
+- Objectif : La liste des variants ayant ad_alt >= 3 dans la table normalized_snv est incluse dans celle de la table normalized_variants
+'''
 
-```
-this is a code block
-```
+same_list_snv_variants = '''
+### Documentation
+- Test : Liste des variants - Entre les tables normalized_snv et variants
+- Objectif : La liste des variants ayant ad_alt >= 3 dans la table normalized_snv est incluse dans celle de la table variants
+'''
+
+same_list_variants_variant_centric = '''
+### Documentation
+- Test : Liste des variants - Entre les tables variants et variant_centric
+- Objectif : La liste des variants dans la table variants est la même que dans la table variant_centric
 '''
--- dags/lib/groups/qc.py
+++ dags/lib/groups/qc.py
@@ -24,6 +24,7 @@ def qc(
 
         no_dup_nor_variants = SparkOperator(
             task_id='no_dup_nor_variants',
+            doc_md=doc.no_dup_nor_variants,
             name='etl-qc-no-dup-nor-variants',
             k8s_context=K8sContext.ETL,
             spark_class='bio.ferlab.clin.etl.qc.variantlist.NonDuplicationNorVariants',
@@ -34,6 +35,7 @@ def qc(
 
         no_dup_variants = SparkOperator(
             task_id='no_dup_variants',
+            doc_md=doc.no_dup_variants,
             name='etl-qc-no-dup-variants',
             k8s_context=K8sContext.ETL,
             spark_class='bio.ferlab.clin.etl.qc.variantlist.NonDuplicationVariants',
@@ -44,6 +46,7 @@ def qc(
 
         no_dup_variant_centric = SparkOperator(
             task_id='no_dup_variant_centric',
+            doc_md=doc.no_dup_variant_centric,
             name='etl-qc-no-dup-variant-centric',
             k8s_context=K8sContext.ETL,
             spark_class='bio.ferlab.clin.etl.qc.variantlist.NonDuplicationVariantCentric',
@@ -54,6 +57,7 @@ def qc(
 
         same_list_snv_nor_variants = SparkOperator(
             task_id='same_list_snv_nor_variants',
+            doc_md=doc.same_list_snv_nor_variants,
             name='etl-qc-same-list-snv-nor-variants',
             k8s_context=K8sContext.ETL,
             spark_class='bio.ferlab.clin.etl.qc.variantlist.SameListBetweenSNVAndNorVariants',
@@ -64,6 +68,7 @@ def qc(
 
         same_list_snv_variants = SparkOperator(
             task_id='same_list_snv_variants',
+            doc_md=doc.same_list_snv_variants,
             name='etl-qc-same-list-snv-variants',
             k8s_context=K8sContext.ETL,
             spark_class='bio.ferlab.clin.etl.qc.variantlist.SameListBetweenSNVAndVariants',
@@ -74,6 +79,7 @@ def qc(
 
         same_list_variants_variant_centric = SparkOperator(
             task_id='same_list_variants_variant_centric',
+            doc_md=doc.same_list_variants_variant_centric,
             name='etl-qc-same-list-variants-variant-centric',
             k8s_context=K8sContext.ETL,
             spark_class='bio.ferlab.clin.etl.qc.variantlist.SameListBetweenVariantsAndVariantCentric',
