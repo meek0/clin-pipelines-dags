734d99a875fcc6bb0ce098c7e49eb6fe710922f5
jecos <jcostanza@ferlab.bio>
2022-10-13 17:20:56 -0400

    fix: CLIN-1361 update clin-variant-etl jar

--- dags/lib/config.py
+++ dags/lib/config.py
@@ -37,7 +37,7 @@ spark_service_account = 'spark'
 
 if env == Env.QA:
     es_url = 'http://elasticsearch:9200'
-    spark_jar = 'https://github.com/Ferlab-Ste-Justine/clin-variant-etl/releases/download/v2.3.32/clin-variant-etl.jar'
+    spark_jar = 'https://github.com/Ferlab-Ste-Justine/clin-variant-etl/releases/download/v2.3.33/clin-variant-etl.jar'
     ca_certificates = 'ingress-ca-certificate'
 elif env == Env.STAGING:
     es_url = 'http://elasticsearch:9200'
