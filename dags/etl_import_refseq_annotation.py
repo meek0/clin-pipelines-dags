2e71a48f9967698fe1eab99519498713406a5379
Matt Syms <matthieu.symoens@gmail.com>
2022-10-17 18:15:37 -0400

    feat: CLIN-1361 added refseq_annotation import

---
+++ dags/etl_import_refseq_annotation.py
@@ -0,0 +1,77 @@
+import logging
+import re
+from airflow import DAG
+from airflow.exceptions import AirflowSkipException
+from airflow.operators.python import PythonOperator
+from airflow.providers.amazon.aws.hooks.s3 import S3Hook
+from airflow.utils.trigger_rule import TriggerRule
+from datetime import datetime
+from lib import config
+from lib.config import env, K8sContext
+from lib.operators.spark import SparkOperator
+from lib.slack import Slack
+from lib.utils import http_get, http_get_file
+
+
+with DAG(
+    dag_id='etl_import_refseq_annotation',
+    start_date=datetime(2022, 1, 1),
+    schedule_interval=None,
+    default_args={
+        'on_failure_callback': Slack.notify_task_failure,
+    },
+) as dag:
+
+    def _file():
+        refseq_url = 'https://ftp.ncbi.nlm.nih.gov/genomes/refseq/vertebrate_mammalian/Homo_sapiens/annotation_releases/current'
+        refseq_file = 'GCF_GRCh38_genomic.gff.gz'
+
+        s3 = S3Hook(config.s3_conn_id)
+        s3_bucket = f'cqgc-{env}-app-datalake'
+        s3_key = f'raw/landing/refseq/{refseq_file}'
+
+        # Get latest version
+        html = http_get(refseq_url).text
+        latest_ver = re.search('>GCF_(.+_GRCh38.+)/<', html).group(1)
+        logging.info(f'Refseq Annotation latest version: {latest_ver}')
+
+        # Get imported version
+        imported_ver = None
+        if s3.check_for_key(f'{s3_key}.version', s3_bucket):
+            imported_ver = s3.read_key(f'{s3_key}.version', s3_bucket)
+        logging.info(f'Refseq Annotation imported version: {imported_ver}')
+
+        # Skip task if up to date
+        if imported_ver == latest_ver:
+            raise AirflowSkipException()
+
+        # Download file
+        http_get_file(
+            f'{refseq_url}/GCF_{latest_ver}/GCF_{latest_ver}_genomic.gff.gz', refseq_file
+        )
+
+        # Upload file to S3
+        s3.load_file(refseq_file, s3_key, s3_bucket, replace=True)
+        s3.load_string(
+            latest_ver, f'{s3_key}.version', s3_bucket, replace=True
+        )
+        logging.info(f'New Refseq Annotation imported version: {latest_ver}')
+
+    file = PythonOperator(
+        task_id='file',
+        python_callable=_file,
+        on_execute_callback=Slack.notify_dag_start,
+    )
+
+    table = SparkOperator(
+        task_id='table',
+        name='etl-import-refseq-annotation-table',
+        k8s_context=K8sContext.ETL,
+        spark_class='bio.ferlab.datalake.spark3.publictables.ImportPublicTable',
+        spark_config='enriched-etl',
+        arguments=['refseq_annotation'],
+        trigger_rule=TriggerRule.ALL_SUCCESS,
+        on_success_callback=Slack.notify_dag_completion,
+    )
+
+    file >> table
