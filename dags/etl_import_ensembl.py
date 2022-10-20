4da023629389ba2ba5e88c314846adb2ee4dfb7a
Yann MARCOU <creativeyann17@gmail.com>
2022-10-20 12:25:19 -0400

    feat: CLIN-1500 - add version logic

--- dags/etl_import_ensembl.py
+++ dags/etl_import_ensembl.py
@@ -1,4 +1,5 @@
 import logging
+import re
 from airflow import DAG
 from airflow.exceptions import AirflowSkipException
 from airflow.operators.python import PythonOperator
@@ -9,7 +10,7 @@ from lib import config
 from lib.config import env, K8sContext
 from lib.operators.spark import SparkOperator
 from lib.slack import Slack
-from lib.utils_import import get_s3_file_md5, download_and_check_md5, load_to_s3_with_md5
+from lib.utils_import import get_s3_file_version, download_and_check_md5, load_to_s3_with_version
 
 
 with DAG(
@@ -21,33 +22,48 @@ with DAG(
     },
 ) as dag:
 
+    def find_last_version(checksums: str, type: str) -> str:
+        file = open(checksums, 'r')
+        lines = file.readlines()
+        file.close()
+        for line in lines:
+            version = re.search(f'Homo_sapiens.GRCh38.([0-9_]+)\.{type}.tsv.gz', line)
+            if version is not None:
+                return version.group(1)
+        return None
+
+
     def _file():
         url = 'http://ftp.ensembl.org/pub/current_tsv/homo_sapiens'
         types = ['canonical', 'ena', 'entrez', 'refseq', 'uniprot']
+        checksums = 'CHECKSUMS'
         updated = False
 
+        download_and_check_md5(url, checksums, None)
+  
         for type in types:
 
-            file = f'Homo_sapiens.GRCh38.108.{type}.tsv.gz'
+            file = f'Homo_sapiens.GRCh38.{type}.tsv.gz' # without version
 
             s3 = S3Hook(config.s3_conn_id)
             s3_bucket = f'cqgc-{env}-app-datalake'
             s3_key = f'raw/landing/ensembl/{file}'
 
             # Get latest s3 MD5 checksum
-            s3_md5 = get_s3_file_md5(s3, s3_bucket, s3_key)
-            logging.info(f'Current {type} imported MD5 hash: {s3_md5}')
+            s3_version = get_s3_file_version(s3, s3_bucket, s3_key)
+            logging.info(f'Current {type} imported version: {s3_version}')
 
-            # Download file
-            download_md5 = download_and_check_md5(url, file, None)
+            new_version = find_last_version(checksums, type)
 
-            # Verify MD5 checksum
-            if download_md5 != s3_md5:
+            if s3_version != new_version:
+                # Download file with version
+                file_with_version = f'Homo_sapiens.GRCh38.{new_version}.{type}.tsv.gz'
+                download_and_check_md5(url, file_with_version, None)
+
                 # Upload file to S3
-                load_to_s3_with_md5(s3, s3_bucket, s3_key, file, download_md5)
-                logging.info(f'New {type} imported MD5 hash: {download_md5}')
+                load_to_s3_with_version(s3, s3_bucket, s3_key, file_with_version, new_version)
+                logging.info(f'New {type} imported version: {new_version}')
                 updated = True
-
 
         if not updated:
             raise AirflowSkipException()
