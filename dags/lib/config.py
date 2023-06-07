e2d2fa67906d67fd8d0735c38fb403f8f62d8b03
Yann MARCOU <creativeyann17@gmail.com>
2023-06-07 16:05:18 -0400

    feat: CLIN-1998 release

--- dags/lib/config.py
+++ dags/lib/config.py
@@ -50,7 +50,7 @@ elif env == Env.STAGING:
     pipeline_image = 'ferlabcrsj/clin-pipelines:4ddf4ef'
     panels_image = 'ferlabcrsj/clin-panels:13b8182d493658f2c6e0583bc275ba26967667ab-1683653903'
     es_url = 'http://elasticsearch:9200'
-    spark_jar = 's3a://cqgc-staging-app-datalake/jars/clin-variant-etl-v2.7.6.jar'
+    spark_jar = 's3a://cqgc-staging-app-datalake/jars/clin-variant-etl-v2.9.3.jar'
     ca_certificates = 'ingress-ca-certificate'
     minio_certificate = 'minio-ca-certificate'
     indexer_context = K8sContext.DEFAULT
