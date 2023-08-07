dee8ce6fd1cf4833bdc7e2298233db2f13f8007c
Yann MARCOU <creativeyann17@gmail.com>
2023-08-07 14:52:33 -0400

    feat: CLIN-2124 - release

--- dags/lib/config.py
+++ dags/lib/config.py
@@ -57,11 +57,11 @@ elif env == Env.STAGING:
     indexer_context = K8sContext.DEFAULT
     auth_url = 'https://auth.staging.cqgc.hsj.rtss.qc.ca/auth'
 elif env == Env.PROD:
-    fhir_image = 'ferlabcrsj/clin-fhir:2a72663'
-    pipeline_image = 'ferlabcrsj/clin-pipelines:f470295'
+    fhir_image = 'ferlabcrsj/clin-fhir:fc5878d'
+    pipeline_image = 'ferlabcrsj/clin-pipelines:d6ecde4'
     panels_image = 'ferlabcrsj/clin-panels:13b8182d493658f2c6e0583bc275ba26967667ab-1683653903'
     es_url = 'https://workers.search.cqgc.hsj.rtss.qc.ca:9200'
-    spark_jar = 's3a://cqgc-prod-app-datalake/jars/clin-variant-etl-v2.10.0.jar'
+    spark_jar = 's3a://cqgc-prod-app-datalake/jars/clin-variant-etl-v2.13.5.jar'
     ca_certificates = 'ca-certificates-bundle'
     minio_certificate = 'ca-certificates-bundle'
     indexer_context = K8sContext.ETL
