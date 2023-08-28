12c0abfc562abfc07360c73a9c0a13bbabb1efc4
Laura Bégin <lbegin@ferlab.bio>
2023-08-28 16:50:12 -0400

    feat: CLIN-2182 Download cosmic mutation census file (#220)

--- dags/etl_import_cosmic.py
+++ dags/etl_import_cosmic.py
@@ -16,57 +16,75 @@ from lib.utils import http_get, http_get_file
 from lib.utils_import import get_s3_file_version, load_to_s3_with_version
 
 with DAG(
-    dag_id='etl_import_cosmic',
-    start_date=datetime(2022, 1, 1),
-    schedule_interval=None,
-    default_args={
-        'on_failure_callback': Slack.notify_task_failure,
-    },
+        dag_id='etl_import_cosmic',
+        start_date=datetime(2022, 1, 1),
+        schedule_interval=None,
+        default_args={
+            'on_failure_callback': Slack.notify_task_failure,
+        },
 ) as dag:
+    def get_latest_ver(url):
+        html = http_get(url).text
+        latest_ver = re.search('COSMIC (v[0-9]+),', html).group(1)
+        logging.info(f'COSMIC latest version: {latest_ver}')
+        return latest_ver
 
-    def _file():
+
+    def _files():
         url = 'https://cancer.sanger.ac.uk/cosmic'
         path = 'file_download/GRCh38/cosmic'
-        file = 'cancer_gene_census.csv'
 
         s3 = S3Hook(config.s3_conn_id)
         s3_bucket = f'cqgc-{env}-app-datalake'
-        s3_key = f'raw/landing/cosmic/{file}'
 
         # Get latest version
         html = http_get(url).text
         latest_ver = re.search('COSMIC (v[0-9]+),', html).group(1)
         logging.info(f'COSMIC latest version: {latest_ver}')
 
-        # Get imported version
-        imported_ver = get_s3_file_version(s3, s3_bucket, s3_key)
-        logging.info(f'COSMIC imported version: {imported_ver}')
+        gene_census_file = 'cancer_gene_census.csv'
+        mutation_census_file = f'CancerMutationCensus_AllData_Tsv_{latest_ver}_GRCh38.tar'
+        updated = False
 
-        # Skip task if up to date
-        if imported_ver == latest_ver:
+        for file_name in [gene_census_file, mutation_census_file]:
+            s3_key = f'raw/landing/cosmic/{file_name}'
+
+            # Get imported version
+            imported_ver = get_s3_file_version(s3, s3_bucket, s3_key)
+            logging.info(f'Current file {file_name} imported version: {imported_ver}')
+
+            # Skip task if up to date
+            if imported_ver == latest_ver:
+                f'Skipping import of file {file_name}. Imported version is up to date.'
+                continue
+
+            # Encode credentials
+            credentials = base64.b64encode(
+                config.cosmic_credentials.encode()
+            ).decode()
+
+            # Get download url
+            download_url = http_get(
+                f'{url}/{path}/{latest_ver}/{file_name}',
+                {'Authorization': f'Basic {credentials}'}
+            ).json()['url']
+
+            # Download file
+            http_get_file(download_url, file_name)
+
+            # Upload file to S3
+            load_to_s3_with_version(s3, s3_bucket, s3_key, file_name, latest_ver)
+            logging.info(f'New file {file_name} imported version: {latest_ver}')
+            updated = True
+
+        # If no files have been updated, skip task
+        if not updated:
             raise AirflowSkipException()
 
-        # Encode credentials
-        credentials = base64.b64encode(
-            config.cosmic_credentials.encode()
-        ).decode()
 
-        # Get download url
-        download_url = http_get(
-            f'{url}/{path}/{latest_ver}/{file}',
-            {'Authorization': f'Basic {credentials}'}
-        ).json()['url']
-
-        # Download file
-        http_get_file(download_url, file)
-
-        # Upload file to S3
-        load_to_s3_with_version(s3, s3_bucket, s3_key, file, latest_ver)
-        logging.info(f'New COSMIC imported version: {latest_ver}')
-
-    file = PythonOperator(
-        task_id='file',
-        python_callable=_file,
+    files = PythonOperator(
+        task_id='files',
+        python_callable=_files,
         on_execute_callback=Slack.notify_dag_start,
     )
 
@@ -85,4 +103,6 @@ with DAG(
         on_success_callback=Slack.notify_dag_completion,
     )
 
-    file >> table
+    # TODO: Import mutation census public table
+
+    files >> table
