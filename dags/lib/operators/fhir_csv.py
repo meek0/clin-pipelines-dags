2684de9eccbd4ef895a9372217215cdd9c93b22a
Yann MARCOU <creativeyann17@gmail.com>
2024-05-28 15:23:08 -0400

    feat: CLIN-2886 improt fhir config

--- dags/lib/operators/fhir_csv.py
+++ dags/lib/operators/fhir_csv.py
@@ -1,19 +1,22 @@
-from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
+from airflow.exceptions import AirflowSkipException
+from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import \
+    KubernetesPodOperator
 from kubernetes.client import models as k8s
 from lib import config
-from lib.config import env_url, auth_url
+from lib.config import auth_url, env_url
 from lib.utils import join
 
 
 class FhirCsvOperator(KubernetesPodOperator):
 
     template_fields = KubernetesPodOperator.template_fields + (
-        'color',
+        'color','skip'
     )
 
     def __init__(
         self,
         k8s_context: str,
+        skip: bool = False,
         color: str = '',
         **kwargs,
     ) -> None:
@@ -26,9 +29,13 @@ class FhirCsvOperator(KubernetesPodOperator):
             image=config.fhir_csv_image,
             **kwargs,
         )
+        self.skip = skip
         self.color = color
 
     def execute(self, **kwargs):
+
+        if self.skip:
+            raise AirflowSkipException()
 
         self.image_pull_secrets = [
             k8s.V1LocalObjectReference(
