73fd828bddaf2158f68c5a084a1dec6dc78e2250
Yann MARCOU <creativeyann17@gmail.com>
2024-07-15 19:51:16 -0400

    feat: CLIN-3006 remove qc es schedule

--- dags/etl_qc_es.py
+++ dags/etl_qc_es.py
@@ -12,7 +12,8 @@ from lib.utils_etl import spark_jar
 with DAG(
     dag_id='etl_qc_es',
     start_date=datetime(2022, 1, 1),
-    schedule_interval='0 1 * * *',
+    schedule_interval=None,
+    #schedule_interval='0 1 * * *',
     catchup=False,  # IF true the dag will run every missing schedule_interval since start_date
     params={
 
